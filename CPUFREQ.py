9d091c552dca30e097ec78312d442265d48e030e
V8Wookiee <33057685+V8Wookiee@users.noreply.github.com>
2017-10-27 20:36:37 +0800

    Update CPUFREQ.py
    Fixed LCD output to one line

--- CPUFREQ.py
+++ CPUFREQ.py
@@ -1,8 +1,15 @@
- # CPU Frequency
+def get_cpu_freq():
+    process = Popen(['vcgencmd', 'measure_clock arm'], stdout=PIPE)
+    output, _error = process.communicate()
+
+    match = re.search(r'=(\d+)', output)
+    freq = match.group(1)
+    # print("freq = '" + freq + "'\n")
+
+# CPU Frequency
         cpu_freq = get_cpu_freq()
         freq = int(cpu_freq)
-        LINE1 = "CPU Freq="
-        LINE2 = str(freq) + "Mhz"
+        LINE1 = "CPU =    " + str(freq / 1024 / 1024) + "Mhz"
+        # LINE2 =
         lcd_string(LINE1, LCD_LINE_1)
         lcd_string(LINE2, LCD_LINE_2)
-        time.sleep(4)
