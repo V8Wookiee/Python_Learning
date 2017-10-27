8f8b229b4cb0ac65abfc87b85f53e0ee66571fc3
V8Wookiee <33057685+V8Wookiee@users.noreply.github.com>
2017-10-27 21:58:20 +0800

    Update UptimeTest.py

--- UptimeTest.py
+++ UptimeTest.py
@@ -1,8 +1,15 @@
- # Uptime Info
-        subprocess.check_output(['uptime', '-p'])
+# Uptime Info
+        test = subprocess.check_output(['uptime', '-p'])
+        print "test = '" + test + "'\n"
+
+        match = re.search(r'up (.*)', test)
+        var = match.group(1)
+
+        match = re.search(r'^(.{0,16})', var)
+        var = match.group(1)
 
         LINE1 = "UPTIME:"
-        LINE2 = str(uptime_string)
+        LINE2 = str(test)
         lcd_string(LINE1, LCD_LINE_1)
         lcd_string(LINE2, LCD_LINE_2)
 
